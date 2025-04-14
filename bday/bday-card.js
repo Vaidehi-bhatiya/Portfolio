b082bd45348a794ffead2ec0c31b45794d1d1da7
Vaidehi-bhatiya <vaidehibhatiya1396@gmail.com>
2025-04-15 00:37:55 +0530

    Add files via upload

---
+++ bday/bday-card.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+    // $(".srch").mouseover(function(){
+    //     $(".drop-srch").show();
+    // });
+    // $(".drop-srch").mouseleave(function(){
+    //     $(".drop-srch").hide();
+    // })
+
+
+    $(".wedding").mouseover(function(){
+        $(".box1").show();
+    });
+    $(".box1").mouseleave(function(){
+        $(".box1").hide();
+    });
+
+    $(".birthday").mouseover(function(){
+        $(".box2").show();
+    });
+    $(".box2").mouseleave(function(){
+        $(".box2").hide();
+    });
+
+    $(".kids").mouseover(function(){
+        $(".box3").show();
+    });
+    $(".box3").mouseleave(function(){
+        $(".box3").hide();
+    });
+
+    $(".party").mouseover(function(){
+        $(".box4").show();
+    });
+    $(".box4").mouseleave(function(){
+        $(".box4").hide();
+    });
+
+    $(".gretting").mouseover(function(){
+        $(".box5").show();
+    });
+    $(".box5").mouseleave(function(){
+        $(".box5").hide();
+    });
+
+    $(".trending").mouseover(function(){
+        $(".box6").show();
+    });
+    $(".box6").mouseleave(function(){
+        $(".box6").hide();
+    });
+
+    $(".srch").mouseover(function(){
+        $(".box7").show();
+    });
+    $(".box7").mouseleave(function(){
+        $(".box7").hide();
+    });
+
+    $(".img-box1").mouseover(function(){
+        $(".drop-img1").show();
+    });
+    $(".drop-img1").mouseleave(function(){
+        $(".drop-img1").hide();
+    });
+});
